baf92df15754ea9bebaf46204441f3d807ba1378
unknown <tho.giotti@gmail.com>
2019-10-30 11:46:23 +0100

    Ajout des media queries 2024px 900px et 680px

--- js/js.js
+++ js/js.js
@@ -8,17 +8,18 @@ $(document).ready(function () {
         $('#menu').toggleClass('expanded');
         $('body').css('background',)
     });
-    let input = $('input[type=search]');
-    $('label[for=test]').on('click',function(){
-        if ( input.width() === 0) {
-            input.css('width', '150px');
-            input.show()
-        }else{
-            input.css('width','0');
-            input.delay(750).hide(0);
-
-        }});
-
+    if ($(window).width() < 1024) {
+        let input = $('input[type=search]');
+        $('label[for=test]').on('click', function () {
+            if (input.width() === 0) {
+                input.css('width', '150px');
+                input.show()
+            } else {
+                input.css('width', '0');
+                input.delay(750).hide(0);
+            }
+        });
+    }
 
 });
 
